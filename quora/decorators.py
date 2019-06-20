fdfd6c5b553f3e5bb9edbabcf70f71150983c0e0
Srikanta Sanka <srikantasanka@Srikantas-MacBook-Air.local>
2019-06-20 14:30:28 +0530

    removed set_trace in decorator

--- quora/decorators.py
+++ quora/decorators.py
@@ -1,9 +1,7 @@
 from django.core.exceptions import PermissionDenied
 from quora.models import Post
-import pdb
 def user_is_post_author(function):
     def wrap(request, *args, **kwargs):
-        pdb.set_trace()
         post = Post.objects.get(pk=kwargs['post_id'])
         if post.user.user_name == request.session['user']:
             return function(request, *args, **kwargs)
